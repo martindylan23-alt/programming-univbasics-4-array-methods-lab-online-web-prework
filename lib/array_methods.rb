df32eae74f0a22999097d09336a26588760d5309
Dyla n ra y <technicalpro@securemedia.ch>
2020-01-21 16:12:40 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,7 +1,7 @@
 def using_include(array, element)
   array = ["wow", "I", "am", "really", "learning", "arrays!"]
-    array.include? ['a']
-    array.include? ['wow']
+    array.include? ('a')
+    array.include? ('wow')
     
 end
 
